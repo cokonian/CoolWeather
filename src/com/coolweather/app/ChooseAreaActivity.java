f8c2f3f569864af5ab93af1cf8f2ff94ec77efe1
cokonian <cokonian@gmail.com>
2015-10-01 11:11:10 +0800

    added change city and weatherinfo refresh

--- src/com/coolweather/app/ChooseAreaActivity.java
+++ src/com/coolweather/app/ChooseAreaActivity.java
@@ -37,13 +37,15 @@ public class ChooseAreaActivity extends Activity
     private Province selectedProvince;
     private City selectedCity;
     private int currentLevel;
+    private boolean isFromWeatherActivity;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
     	super.onCreate(savedInstanceState);
+    	isFromWeatherActivity=getIntent().getBooleanExtra("from_weather_activity",false);
     	SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
-    	if(prefs.getBoolean("city_selected", false))
+    	if(prefs.getBoolean("city_selected", false)&&!isFromWeatherActivity)
     	{
     		Intent i=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
     		startActivity(i);
@@ -231,6 +233,11 @@ public class ChooseAreaActivity extends Activity
     	{
     		queryProvinces();
     	}else{
+    		if(isFromWeatherActivity)
+    		{
+    			Intent intent=new Intent(this,WeatherActivity.class);
+    			startActivity(intent);
+    		}
     		finish();
     	}
     }
